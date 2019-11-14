97b5d79e366725691002ae339816eb6e0650b465
Dimitris Athanasiadis <34347336+DimitrisAthanasiadis@users.noreply.github.com>
2019-11-14 23:26:18 +0200

    I just pushed the files I worked today.

--- guestbook.py
+++ guestbook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
@@ -21,124 +21,189 @@ db = SQLAlchemy(app)
 
 # class that is used to create the database
 class Comments(db.Model):
-    # db columns
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    comment = db.Column(db.String(1000))
+	# db columns
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+	comment = db.Column(db.String(1000))
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
-    password = db.Column(db.String(100))
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20))
+	password = db.Column(db.String(100))
 
 
 # create a route
 @app.route('/')
 def index():
-    # list of sql alchemy objects
-    result = Comments.query.all()
+	# list of sql alchemy objects
+	result = Comments.query.all()
 
-    return render_template('index.html', result=result)
+	return render_template('index.html', result=result)
 
 
 @app.route('/sign')
 def sign():
-    return render_template('sign.html')
+	return render_template('sign.html')
 
 
 @app.route('/comment_process', methods=['GET', 'POST'])
 def comment_process():
-    # it takes the name from the form and assigns it to the variable name
-    name = request.form['name']
-    # it takes the comment from the form and assigns it to the variable comment
-    comment = request.form['comment']
+	# it takes the name from the form and assigns it to the variable name
+	name = request.form['name']
+	# it takes the comment from the form and assigns it to the variable comment
+	comment = request.form['comment']
+	session['con_cont'] = comment
 
-    # instantiates the object-creates row
-    signature = Comments(name=name, comment=comment)
-    # adds the row
-    db.session.add(signature)
-    # saves the changes
-    db.session.commit()
+	# instantiates the object-creates row
+	signature = Comments(name=name, comment=comment)
+	# adds the row
+	db.session.add(signature)
+	# saves the changes
+	db.session.commit()
 
-    # redirects to index so it updates
-    return redirect(url_for('index'))
+	# redirects to index so it updates
+	return redirect(url_for('index'))
 
 
 @app.route('/login_process', methods=['POST'])
 def login_process():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    # exists = bool(Users.query.filter_by(username=username).first())
-    exists = db.session.query(db.session.query(Users).filter_by(username=username).exists()).scalar()
-    logged_in = False
+	error = None
+	username = request.form.get('username')
+	password = request.form.get('password')
+	# exists = bool(Users.query.filter_by(username=username).first())
+	exists = db.session.query(db.session.query(Users).filter_by(username=username).exists()).scalar()
+	logged_in = False
 
-    if exists == True:
-        logged_in = True
-        session['username'] = username
-        session['password'] = password
-        session['logged_in'] = logged_in
+	if exists == True:
+		logged_in = True
+		session['username'] = username
+		session['password'] = password
+		session['logged_in'] = logged_in
+		flash("Successful login", "success")
+		return redirect(url_for("index"))
+	else:
+		flash("Invalid credentials", "error")
 
-    return redirect(url_for('index'))
+	return redirect(url_for("login"))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+	return render_template('login.html')
 
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template('sign_up.html')
+	return render_template('sign_up.html')
 
 
 @app.route('/sign_up_process', methods=['POST'])
 def sign_up_process():
-    username = request.form['username']
-    password = request.form['password']
+	username = request.form['username']
+	password = request.form['password']
+	exists = db.session.query(db.session.query(Users).filter_by(username=username).exists()).scalar()
 
-    user = Users(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
 
-    return redirect(url_for('index'))
+	if exists == True:
+		flash("You cannot use this username", "error")
+		return redirect(url_for('sign_up'))
+
+	user = Users(username=username, password=password)
+	db.session.add(user)
+	db.session.commit()
+	session['logged_in'] = True
+	session['username'] = username
+	session['password'] = password
+
+	return redirect(url_for('index'))
+
+
+@app.route("/logout")
+def logout():
+	session["logged_in"] = False
+	session.pop("username", None)
+	session.pop("password", None)
+
+	return redirect(url_for('index'))
+
+@app.route('/edit_com')
+def edit_com():
+	render_template("edit.html")
+
+
+@app.route("/edit_com_process", methods=['GET', 'POST'])
+def edit_com_process():
+	com_id = request.args.get('com_id')
+	com_cont = session['com_cont']
+	query = text("update comments set comment = " + str(com_cont) + " where id = " + str(com_id))
+	db.engine.execute(query)
+	db.session.commit()
+
+	return redirect(url_for('index'))
 
 
 # made with redirect method
 @app.route('/delete_red/', methods=['GET', 'POST'])
 def delete_red():
-    # i should use AJAX so that i do not redirect
-    if request.method == 'POST':
-        comId = request.args.get('id')
-        query = text("delete from comments where id=" + str(comId))
-        db.engine.execute(query)
-        db.session.commit()
+	# i should use AJAX so that i do not redirect
+	if request.method == 'POST':
+		comId = request.args.get('id')
+		query = text("delete from comments where id=" + str(comId))
+		db.engine.execute(query)
+		db.session.commit()
 
-        return redirect(url_for('index'))
-    else:
-        return render_template('index.html')
+		return redirect(url_for('index'))
+	else:
+		return render_template('index.html')
 
 
 # uses AJAX to delete
 @app.route('/delete_ajax', methods=['GET', 'POST'])
 def delete_ajax():
-    com_id = request.args.get('com_id')
-    query = "delete from comments where id=" + str(com_id)
-    db.engine.execute(query)
-    db.session.commit()
+	com_id = request.args.get('com_id')
+	query = "delete from comments where id=" + str(com_id)
+	session.pop("comment", None)
+	db.engine.execute(query)
+	db.session.commit()
 
-    return jsonify(status="success")
+	return jsonify(status="success")
 
 
 # return redirect(url_for('index'))
 
+@app.route('/delete_user_ajax', methods=['GET', 'POST'])
+def delete_user_ajax():
+	u_id = request.args.get('u_id')
+	query = "delete from users where id=" + str(u_id)
+	db.engine.execute(query)
+	db.session.commit()
+
+	return jsonify(status="success")
+
+@app.route('/users_list', methods=['GET', 'POST'])
+def users_list():
+	if session.get('username') is not None:
+		if session['username'] == 'admin':
+			# return redirect(url_for('users_list'))
+			return display_users()
+	else:
+		return render_template('you_are_not_admin.html')
+
+@app.route("/display_users", methods=['GET', 'POST'])
+def display_users():
+	if session.get('username') is not None:
+		result = Users.query.all()
+
+		return render_template("users.html", result=result)
+
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    links = ['https://www.google.gr', 'https://www.youtube.com', 'https://www.facebook.com', 'https://www.twitter.com']
-    # it searches for a file in the default path "./templates/FILE_NAME" to open it
-    # the render_template function does the searching job
-    return render_template('example.html', links=links)
+	links = ['https://www.google.gr', 'https://www.youtube.com', 'https://www.facebook.com', 'https://www.twitter.com']
+	# it searches for a file in the default path "./templates/FILE_NAME" to open it
+	# the render_template function does the searching job
+	return render_template('example.html', links=links)
 
 
 # <VARIABLE_NAME> is a variable that is passed through the URL bar
@@ -148,4 +213,4 @@ def home():
 
 # if i call it from command line (which i do), then debug mode is on
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
